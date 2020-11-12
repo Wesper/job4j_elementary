0213738deea2b0e1152376a5f92586511beca516
artembelanko <wespermc@gmail.com>
2020-11-12 21:43:07 +0500

    3. Перегрузить метод max для трех чисел [#291020]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -6,4 +6,12 @@ public class Max {
         int result = compare ? left : right;
         return result;
     }
+
+    public static int max(int one, int two, int three) {
+        return max(one, max(two, three));
+    }
+
+    public static int max(int one, int two, int three, int four) {
+        return max(one, two, max(three, four));
+    }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -8,21 +8,21 @@ import static org.hamcrest.core.Is.is;
 public class MaxTest {
 
     @Test
-    public void whenMax1To2Then2() {
+    public void maxTwoNumber() {
         int result = Max.max(1, 2);
         assertThat(result, is(2));
     }
 
     @Test
-    public void whenMax2To1Then1() {
-        int result = Max.max(2, 1);
-        assertThat(result, is(2));
+    public void maxThreeNumber() {
+        int result = Max.max(2, 1, 5);
+        assertThat(result, is(5));
     }
 
     @Test
-    public void whenMax1To1Then1() {
-        int result = Max.max(1, 1);
-        assertThat(result, is(1));
+    public void maxFourNumber() {
+        int result = Max.max(1, 3, 2, 9);
+        assertThat(result, is(9));
     }
 
 }
